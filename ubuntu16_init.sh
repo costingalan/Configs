f403a3115d5d49787d906070422a1d3b8a14559e
Costin Galan <cgalan@cloudbasesolutions.com>
2016-12-11 15:28:45 +0200

    Added init script for Ubuntu1604
    Signed-off-by: Costin Galan <cgalan@cloudbasesolutions.com>

---
+++ ubuntu16_init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install vim git wget -y
+useradd cgalan
+usermod -G sudo cgalan
+echo "AllowUsers cgalan" >> /etc/ssh/sshd_config
+echo 'cgalan:super-s3cur3' | chpasswd
+chage -d0 cgalan
+
+apt-get install fail2ban
+touch /etc/fail2ban/jail.local
+echo "[ssh-iptables]" >> /etc/fail2ban/jail.local
+echo "enabled = true" >> /etc/fail2ban/jail.local
+echo "filter = sshd" >> /etc/fail2ban/jail.local
+echo "action = iptables[name=SSH, port=ssh, protocol=tcp]" >> /etc/fail2ban/jail.local
+echo "logpath = /var/log/auth.log" >> /etc/fail2ban/jail.local
+update-rc.d fail2ban defaults
+
+reboot
+
